d49f1df83b8da7373b044552d19c7e7431c37807
bthuntervargr <bthuntervargr@hotmail.com>
2020-02-10 15:08:02 +0200

    comit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Board from'./component/Board';
+import Card from './component/Card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <main className="flexbox">
+       
+     <Board id="board-1" className="board">
+       <Card id="card-1" className="card" draggable="true">
+         <p>Card one</p>
+       </Card>
+     </Board>
+
+     <Board id="board-2" className="board">
+       <Card id="card-2" className="card" draggable="true">
+         <p>Card two</p>
+       </Card>
+     </Board>
+
+     <Board id="board-3" className="board">
+       <Card id="card-3" className="card" draggable="true">
+         <p>Card three</p>
+       </Card>
+     </Board>
+
+     </main>
     </div>
   );
 }
